24b22c50edd5a4b8828b91754cdf510a2cc23307
Tzafqiel <jriem2@uic.edu>
2023-02-06 08:18:44 -0500

    Push
    Push

--- Lab1/fulladder_tb.v
+++ Lab1/fulladder_tb.v
@@ -1,4 +1,4 @@
-module tb_fulladd;
+module fulladder_tb;
 
 reg [1:0] a,b;
 reg cin;
@@ -17,8 +17,6 @@ initial begin
 	a <= 0;
 	b <= 0;
 	cin <= 0;
-	s <= 0;
-	cout <= 0;
 	
 	for (i = 0; i < 25; i = i + 1) begin
 		#10 a <= $random;
@@ -32,8 +30,7 @@ initial
  begin
 	// Open a db file for saving simulation data
   $shm_open ("fulladder_tb.db");
-	// Collect all signals (hierarchically) from the module "adder_tb"
+	// Collect all signals (hierarchically) from the module "fulladder_tb"
   $shm_probe (fulladder_tb,"AS");
  end
-endmodule
 endmodule
